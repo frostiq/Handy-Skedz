dee5dd35e099a87006bbfe4da799a823056da95d
frostiq <sayshayx@gmail.com>
2015-03-10 11:29:31 +0300

    new controllers added

---
+++ jackstimetable_/jackstimetableService/Controllers/ScheduleController.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.WindowsAzure.Mobile.Service;
+using JackstimetableService.DataObjects;
+using JackstimetableService.Models;
+
+namespace JackstimetableService.Controllers
+{
+    public class ScheduleController : TableController<ScheduleDto>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            var context = new JackstimetableContext();
+            DomainManager = new EntityDomainManager<ScheduleDto>(context, Request, Services);
+        }
+
+        // GET tables/Schedule
+        public IQueryable<ScheduleDto> GetAllScheduleDto()
+        {
+            return Query(); 
+        }
+
+        // GET tables/Schedule/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<ScheduleDto> GetScheduleDto(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/Schedule/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<ScheduleDto> PatchScheduleDto(string id, Delta<ScheduleDto> patch)
+        {
+             return UpdateAsync(id, patch);
+        }
+
+        // POST tables/Schedule
+        public async Task<IHttpActionResult> PostScheduleDto(ScheduleDto item)
+        {
+            ScheduleDto current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/Schedule/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteScheduleDto(string id)
+        {
+             return DeleteAsync(id);
+        }
+
+    }
+}
---
+++ jackstimetable_/jackstimetableService/Controllers/SchoolController.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.WindowsAzure.Mobile.Service;
+using JackstimetableService.DataObjects;
+using JackstimetableService.Models;
+
+namespace JackstimetableService.Controllers
+{
+    public class SchoolController : TableController<SchoolDto>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            var context = new JackstimetableContext();
+            DomainManager = new EntityDomainManager<SchoolDto>(context, Request, Services);
+        }
+
+        // GET tables/School
+        public IQueryable<SchoolDto> GetAllSchoolDto()
+        {
+            return Query(); 
+        }
+
+        // GET tables/School/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<SchoolDto> GetSchoolDto(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/School/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<SchoolDto> PatchSchoolDto(string id, Delta<SchoolDto> patch)
+        {
+             return UpdateAsync(id, patch);
+        }
+
+        // POST tables/School
+        public async Task<IHttpActionResult> PostSchoolDto(SchoolDto item)
+        {
+            SchoolDto current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/School/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteSchoolDto(string id)
+        {
+             return DeleteAsync(id);
+        }
+
+    }
+}
